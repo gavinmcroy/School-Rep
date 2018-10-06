51ec53c779bdfec1c92097268e0a96b26039d0a6
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-10-06 10:11:59 -0400

    vectors

--- Learning/Coding Projects/Learning c++/Vectors.cpp
+++ Learning/Coding Projects/Learning c++/Vectors.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+vector <int> numbers (60);
 vector <int> testScores {90,91,95,95,100};
+vector <int> testNumbers (4,50);
 int main(){
-        cout<<testScores[0]<<endl;
-        cout<<testScores[1]<<endl;
-        cout<<testScores[2]<<endl;
-        cout<<testScores[3]<<endl;
-        cout<<testScores[4]<<endl;
-        cout<<testScores.at(2)<<endl;
+        /*  cout<<testScores[0]<<endl;
+           cout<<testScores[1]<<endl;
+           cout<<testScores[2]<<endl;
+           cout<<testScores[3]<<endl;
+           cout<<testScores[4]<<endl;
+           cout<<testScores.at(2)<<endl;
+
+           cout<<"Enter a value"<<endl;
+           cin>>testScores.at(0);
+           cout<<testScores.at(0)<<endl;
+           testScores.push_back(80);
+           cout<<testScores.at(5)<<endl;
+         */
+        cout<<numbers.at(59)<<endl;
+        cout<<testNumbers.at(1)<<endl;
         return 0;
+
 }
