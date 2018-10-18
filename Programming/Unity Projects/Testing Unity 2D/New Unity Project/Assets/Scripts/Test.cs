a54f4a8974c40e6ea76d3ca6075f82526da8b447
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-10-18 07:48:31 -0400

    sourceCode

--- Programming/Unity Projects/Testing Unity 2D/New Unity Project/Assets/Scripts/Test.cs
+++ Programming/Unity Projects/Testing Unity 2D/New Unity Project/Assets/Scripts/Test.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        
+    int min;
+    int max;
+    int guess;
+    void Start () {
+        welcomeScreen(); 
 	}
-	
-	// Update is called once per frame
+	void welcomeScreen()
+    {
+        min = 0;
+        max = 1000;
+        guess = 500;
+        Debug.Log("Guess a number between " + max + " and " + min);
+        Debug.Log("Press 1 for Higher, 2 for Lower and Enter for correct!  ");
+        max++;
+
+    }
 	void Update () {
+        
 
-		
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess; 
+            nextGuess(); 
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            nextGuess();
+        }
+		else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Im brilliant");
+                welcomeScreen();
+        }
 	}
+    void nextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your number " + guess); 
+    }
 }
