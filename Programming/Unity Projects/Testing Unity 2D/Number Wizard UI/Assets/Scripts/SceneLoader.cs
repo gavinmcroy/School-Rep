7f9eb49ef1440e3185730ae6c19d2d977064d24b
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-10-20 21:29:00 -0400

    NumberWizard UI

---
+++ Programming/Unity Projects/Testing Unity 2D/Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneLoader : MonoBehaviour {
+    
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void restartScene()
+    {
+        //---Scene.Manger.LoadScene(0); 
+        int resetCurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(resetCurrentSceneIndex - resetCurrentSceneIndex); 
+    }
+    public void OnApplicationQuit()
+    {
+        Application.Quit(); 
+    }
+}
