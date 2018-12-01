c7d7f647ec1340c1150dae4d5406a3a2b4c75cae
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-12-01 09:05:36 -0500

    java

---
+++ Programming/Coding Projects/C++/FloatandDouble/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myIntValue = 5/3;
+	    float myFloatValue = 5f/3f;
+	    double myDoubleValue = 5d/3d;
+	    System.out.println("My Int Value = "+myIntValue);
+	    System.out.println("My Float Value = "+myFloatValue);
+	    System.out.println("My Double Value = "+myDoubleValue);
+	    System.out.println("Pounds to Kilograms Converter ");
+	    int pounds  = 10;
+	    double kilogramConversion = pounds*.453_592_37d;
+	    System.out.println("Kilograms = "+kilogramConversion);
+    }
+}
