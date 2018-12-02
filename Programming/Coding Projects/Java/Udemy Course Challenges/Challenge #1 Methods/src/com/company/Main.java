c7d2345b9507506a6d422ad12a2df745979a0cf9
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-12-02 11:49:08 -0500

    juicy java

--- Programming/Coding Projects/Java/Udemy Course Challenges/Challenge #1 Methods/src/com/company/Main.java
+++ Programming/Coding Projects/Java/Udemy Course Challenges/Challenge #1 Methods/src/com/company/Main.java
@@ -3,6 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Lets get it");
+
+        int score = calculateHighScorePosition(1000);
+        displayHighScorePosition("Gavin",score);
+
+        score = calculateHighScorePosition(900);
+        displayHighScorePosition("Gavin",score);
+
+        score = calculateHighScorePosition(400);
+        displayHighScorePosition("Gavin",score);
+
+        score = calculateHighScorePosition(50);
+        displayHighScorePosition("Gavin",score);
+
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int tablePosition){
+        System.out.println(playerName+" managed to get into position "+tablePosition);
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore>=1000){
+            return 1;
+        }else if(playerScore>=500 && playerScore<1000){
+            return 2;
+        }else if(playerScore>=100 && playerScore<500){
+            return 3;
+        }
+        return 4;
     }
 }
