5fab9aa5a9794db8a75663d7895e0387c49085c5
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2018-12-13 15:39:33 -0500

    git work

---
+++ Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #5 First and Last Digit Sum/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static int sumFirstAndLastDigit (int number){
+        if(number<0){
+            return -1;
+        }
+        while(number>=1){
+            int lastDigit;
+        }
+    }
+}
