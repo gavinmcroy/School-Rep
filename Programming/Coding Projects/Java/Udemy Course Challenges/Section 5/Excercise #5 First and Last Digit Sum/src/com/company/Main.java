bb50a4040293496dac63e9e0c41a109dddde1921
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-12-13 16:15:13 -0500

    ez

--- Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #5 First and Last Digit Sum/src/com/company/Main.java
+++ Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #5 First and Last Digit Sum/src/com/company/Main.java
@@ -1,16 +1,24 @@
 package com.company;
 
 public class Main {
-
+    //---(252 ---> 7)
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(sumFirstAndLastDigit(1000005));
     }
-    public static int sumFirstAndLastDigit (int number){
-        if(number<0){
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
             return -1;
         }
-        while(number>=1){
-            int lastDigit;
+        int firstDigit = 0;
+        int lastDigit = number;
+
+        while (number >= 1) {
+            firstDigit = number % 10;
+            lastDigit %= 10;
+            number /= 10;
         }
+        System.out.println(firstDigit + "    " + lastDigit);
+        return lastDigit + firstDigit;
     }
 }
