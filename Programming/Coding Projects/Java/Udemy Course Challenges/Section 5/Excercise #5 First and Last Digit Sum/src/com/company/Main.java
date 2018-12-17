c578b78f8593b19dff6c7a6169a2ddf4a1d8485d
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2018-12-16 20:01:00 -0500

    simple java

--- Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #5 First and Last Digit Sum/src/com/company/Main.java
+++ Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #5 First and Last Digit Sum/src/com/company/Main.java
@@ -3,22 +3,5 @@ package com.company;
 public class Main {
     //---(252 ---> 7)
     public static void main(String[] args) {
-        System.out.println(sumFirstAndLastDigit(1000005));
-    }
-
-    public static int sumFirstAndLastDigit(int number) {
-        if (number < 0) {
-            return -1;
-        }
-        int firstDigit = 0;
-        int lastDigit = number;
-
-        while (number >= 1) {
-            firstDigit = number % 10;
-            lastDigit %= 10;
-            number /= 10;
-        }
-        System.out.println(firstDigit + "    " + lastDigit);
-        return lastDigit + firstDigit;
     }
 }
--- Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #7 Shared Digit/src/com/company/Main.java
+++ Programming/Coding Projects/Java/Udemy Course Challenges/Section 5/Excercise #7 Shared Digit/src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(getEvenDigitSum(22));
+    }
+    public static int getEvenDigitSum(int number){
+        if(number<0){
+            return 0;
+        }
+        int evenDigit=1;
+        int evenSum = 0;
+        while(number>=1){
+            number%=10;
+            if(evenDigit%2==0){
+                evenSum+=evenDigit;
+            }
+        }
+        return evenSum;
     }
 }
