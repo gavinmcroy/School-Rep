13f534cf87dcc6b04fc071554dba1fad60c2d603
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2019-02-05 17:17:37 -0500

    AutoBoxing and BS

---
+++ Coding Projects/Java/Udemy Course Challenges/Section 8 Arrays Lists Autoboxing, Unboxing/Challenge #5 Banking Application/src/com/company/Branches.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Branches {
+
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Customers> customers = new ArrayList<>();
+    private String branchName;
+
+    public Branches(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public void addCustomer() {
+        System.out.println("Enter the name of the new user");
+        String name = scanner.nextLine();
+        System.out.println("Enter the transaction amount");
+        boolean isDouble = scanner.hasNextDouble();
+        if (isDouble) {
+            double amount = scanner.nextDouble();
+            customers.add(new Customers(name, amount));
+            System.out.println("New Customer Successfully Added! ");
+        } else {
+            System.out.println("Invalid Input Please Try Again");
+        }
+    }
+
+    public ArrayList<Customers> getCustomers() {
+        return customers;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+}
