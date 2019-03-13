45ac50f3064996c313a5ac4df3f1ca50c8d2a423
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2019-03-13 15:32:52 -0400

    Hot stuff

--- Coding Projects/Java/Juicy Java/tester31/src/com/company/Calculator.java
+++ Coding Projects/Java/Juicy Java/tester31/src/com/company/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
 
     private Scanner scanner = new Scanner(System.in);
     private double totalNumber = 0;
+    private boolean firstStart = true;
 
     public Calculator() {
         mainApplication();
@@ -13,16 +14,20 @@ public class Calculator {
 
     private void mainApplication() {
         mainMenu();
-        determineUserChoice(readUserChoice());
+        while (determineUserChoice(readUserChoice()) != -1) {
+            mainMenu();
+        }
     }
 
     private void mainMenu() {
+        System.out.println("Result: " + this.totalNumber);
         System.out.println("Choose one of the following Options\n" +
                 "1. Add\n" +
                 "2. Subtract\n" +
                 "3. Multiply\n" +
                 "4. Divide\n" +
-                "5. Quit");
+                "5. Quit\n" +
+                "6. Clear Result");
     }
 
     private int readUserChoice() {
@@ -30,49 +35,92 @@ public class Calculator {
         if (hasInt) {
             return scanner.nextInt();
         } else {
+            scanner.nextLine();
             System.out.println("Enter a integer");
             return -1;
         }
     }
 
-    private void determineUserChoice(int userChoice) {
+    private int determineUserChoice(int userChoice) {
         switch (userChoice) {
             case 1:
-                add(recordNumberInput());
+                add();
+                break;
+            case 2:
+                subtract();
+                break;
+            case 3:
+                multiply();
+                break;
+            case 4:
+                divide();
+                break;
+            case 5:
+                return -1;
+            case 6:
+                resetToBeginning();
                 break;
             default:
-                System.out.println("Working!");
+                System.out.println("Enter a valid choice");
         }
+        return 0;
     }
 
-    private void add(double numberToAdd) {
-        //---Adds what ever numbers user enters together until they want to stop
-        //---Keeps track of number until user clears
-        System.out.println(numberToAdd);
+    private void add() {
+        double numberToAdd = firstStart();
         this.totalNumber += numberToAdd;
     }
 
     private void subtract() {
-
+        double numberToSubtract = firstStart();
+        this.totalNumber -= numberToSubtract;
     }
 
     private void multiply() {
-
+        double numberToMultiply = firstStart();
+        this.totalNumber *= numberToMultiply;
     }
 
     private void divide() {
-
-    }
-
-    private double recordNumberInput() {
-        System.out.println("Enter the desired number ");
-        boolean hasDouble = scanner.hasNextDouble();
-        if (hasDouble) {
-            return scanner.nextDouble();
+        double numberToDivide = firstStart();
+        if (numberToDivide == 0) {
+            System.out.println("Error Cannot Divide by 0");
+            resetToBeginning();
         } else {
-            System.out.println("Enter a number please");
-            return -1;
+            this.totalNumber /= numberToDivide;
         }
     }
 
+    private double firstStart() {
+        double number = recordNumberInput();
+        if (this.firstStart) {
+            this.totalNumber = number;
+            this.firstStart = false;
+            number = recordNumberInput();
+        }
+
+        return number;
+    }
+
+    private void resetToBeginning() {
+        System.out.println("Cleared Result");
+        this.totalNumber = 0;
+        this.firstStart = true;
+    }
+
+    private double recordNumberInput() {
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.println("Enter the desired number ");
+            scanner.nextLine();
+            boolean hasDouble = scanner.hasNextDouble();
+            if (hasDouble) {
+                return scanner.nextDouble();
+            } else {
+                System.out.println("Enter a number please");
+                validInput = false;
+            }
+        }
+        return scanner.nextDouble();
+    }
 }
