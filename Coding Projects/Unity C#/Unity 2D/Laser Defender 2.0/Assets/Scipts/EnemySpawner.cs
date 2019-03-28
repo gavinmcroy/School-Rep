4bfd13d44f1a257b3aa0f206b9bf2fb558dcd274
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2019-03-27 20:37:21 -0400

    !!

---
+++ Coding Projects/Unity C#/Unity 2D/Laser Defender 2.0/Assets/Scipts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    private int startWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(
+             waveConfig.GetEnemyPrefab(),
+             waveConfig.GetWayPoints()[0].transform.position,
+             Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
