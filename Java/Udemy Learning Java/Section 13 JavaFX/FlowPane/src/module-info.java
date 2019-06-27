ac75030238f4ecc27bfc8129c4cf49c2dce2963d
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-06-27 16:40:24 -0400

    JavaFX!

---
+++ Java/Udemy Learning Java/Section 13 JavaFX/FlowPane/src/module-info.java
@@ -0,0 +1,8 @@
+module FlowPane {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+
+}
---
+++ Java/Udemy Learning Java/Section 13 JavaFX/HBox Layout/src/module-info.java
@@ -0,0 +1,8 @@
+module HBox.Layout {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+
+}
