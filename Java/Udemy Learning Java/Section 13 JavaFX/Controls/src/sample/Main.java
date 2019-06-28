9a28a5d2570d4282ea0c2a1668d148f1bdf72d4a
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-06-28 08:06:40 -0400

    quite fresh

--- Java/Udemy Learning Java/Section 13 JavaFX/Controls/src/sample/Main.java
+++ Java/Udemy Learning Java/Section 13 JavaFX/Controls/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setTitle("I Call it the Gav Pane");
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
 
