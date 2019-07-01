d4b5e45ae91dd05cf7d892739e5eef3f83acc545
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-07-01 14:24:08 -0400

    Unity is relevent

--- Unity/Unity 2D/Paddle Test/Assets/Scripts/moveScript.cs
+++ Unity/Unity 2D/Paddle Test/Assets/Scripts/moveScript.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveScript : MonoBehaviour {
-    [SerializeField] float screenSize = 16f;
-    [SerializeField] float minX = 1f;
-    [SerializeField] float maxX = 15f;
-	// Use this for initialization
+public class MoveScript : MonoBehaviour {
+    [SerializeField] readonly float screenSize = 16f;
+    [SerializeField] readonly float minX = 1f;
+    [SerializeField] readonly float maxX = 15f;
+	
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
         float mousePos = (Input.mousePosition.x / Screen.width) * screenSize;
-        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePos, minX, maxX);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y)
+        {
+            x = Mathf.Clamp(mousePos, minX, maxX)
+        };
         transform.position = paddlePos;
-
 	}
 }
