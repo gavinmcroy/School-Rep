354db19c4b87d1e36da8eb742ee7df1f11a94bbd
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-07-01 20:15:39 -0400

    Can it handle it !

---
+++ Unity/Unity 2D/Ruby's Adventure/Assets/Scripts/RubyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 144;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 position = transform.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
