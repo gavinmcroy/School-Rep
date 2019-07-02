76eae105804ddd19d8e77494a7eb692d167f8321
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-07-02 19:00:22 -0400

    alot of progress

---
+++ Unity/Unity 2D/Ruby's Adventure/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private float speed = 3.0f;
+    private float timeToSwitch = 3.0f;
+    private float timer;
+    private int direction = 1;
+
+    private bool vertical;
+
+    Animator animator;
+    Rigidbody2D rigidBody2D;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        timer = timeToSwitch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = timeToSwitch;
+        }
+
+        Vector2 position = rigidBody2D.position;
+
+        if (vertical)
+        {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
+            position.y += speed * Time.deltaTime*direction;
+        }
+        else
+        {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
+            position.x += speed * Time.deltaTime*direction;
+        }
+        rigidBody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        RubyController rubyController = collision.gameObject.GetComponent<RubyController>();
+        if (rubyController != null)
+        {
+            rubyController.ChangeHealth(-1);
+        }
+    }
+
+}
--- Unity/Unity 2D/Ruby's Adventure/Assets/Scripts/RubyController.cs
+++ Unity/Unity 2D/Ruby's Adventure/Assets/Scripts/RubyController.cs
@@ -4,23 +4,59 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private readonly float speed = 8.0f;
 
-    [SerializeField] float speed;
+    public int GetMaxHealth { get; } = 5;
+
+    public int GetCurrentHealth { get; private set; }
+
+    private readonly float timeInvicible = 2.0f;
+    private float invicibleTimer;
+    bool isInvicible;
+
+    Rigidbody2D rigidBody2D;
 
     void Start()
     {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        GetCurrentHealth = GetMaxHealth;
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 144;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector2 position = transform.position;
+
+        Vector2 position = rigidBody2D.position;
         position.x += speed * horizontal * Time.deltaTime;
         position.y += speed * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidBody2D.MovePosition(position);
+
+        if (isInvicible)
+        {
+            invicibleTimer -= Time.deltaTime;
+            if (invicibleTimer < 0)
+            {
+                isInvicible = false;
+            }
+        }
+
     }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvicible)
+                return;
+            isInvicible = true;
+            invicibleTimer = timeInvicible;
+        }
+        GetCurrentHealth = Mathf.Clamp(GetCurrentHealth + amount, 0, GetMaxHealth);
+        Debug.Log(GetCurrentHealth + "/" + GetMaxHealth);
+    }
+
 }
