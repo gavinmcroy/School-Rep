9773697495c15463ce817f7d262cde90db89ab6d
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-07-19 11:59:41 -0400

    fresh

--- Unity/Unity 2D/Fresh/Assets/Scripts/Movement.cs
+++ Unity/Unity 2D/Fresh/Assets/Scripts/Movement.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveSpeed = 3;
     Rigidbody2D rigidBody2D;
-    
+    Animator animator;
+
+    private int health = 2;
 
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("MoveX", 1f);
     }
 
     // Update is called once per frame
@@ -19,9 +23,21 @@ public class Movement : MonoBehaviour
         PlayerMovement();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+    }
+
     private void PlayerMovement()
     {
-        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        animator.SetFloat("MoveX",horizontal);
+        animator.SetFloat("Speed", movement.magnitude);
+        //---Does not work Also Animation Instantly Plays Death Anim
+        animator.SetFloat("Health", health);
 
         if (Mathf.Pow(movement.x, 2) + Mathf.Pow(movement.y, 2) > 1)
         {
