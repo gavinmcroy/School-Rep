57ee743bf804f4447879d6324697ab0db467201c
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-07-19 17:49:17 -0400

    Restructuring

--- Unity/Unity 2D/Fresh/Assets/Scripts/Movement.cs
+++ Unity/Unity 2D/Fresh/Assets/Scripts/Movement.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private int health = 2;
     [SerializeField] private float moveSpeed = 3;
     Rigidbody2D rigidBody2D;
     Animator animator;
-
-    private int health = 2;
+    Vector2 movement;
 
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        animator.SetFloat("MoveX", 1f);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        PlayerMovement();
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Death"))
+        {
+
+        }
+        else
+        {
+            PlayerMovement();
+            AnimationController();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,12 +38,7 @@ public class Movement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector2 movement = new Vector2(horizontal, vertical);
-        animator.SetFloat("MoveX",horizontal);
-        animator.SetFloat("Speed", movement.magnitude);
-        //---Does not work Also Animation Instantly Plays Death Anim
-        animator.SetFloat("Health", health);
+        movement = new Vector2(horizontal, vertical);
 
         if (Mathf.Pow(movement.x, 2) + Mathf.Pow(movement.y, 2) > 1)
         {
@@ -48,5 +49,12 @@ public class Movement : MonoBehaviour
 
         position += movement * moveSpeed * Time.deltaTime;
         rigidBody2D.MovePosition(position);
+    }
+
+    private void AnimationController()
+    {
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("Speed", movement.magnitude);
+        animator.SetInteger("Health", health);
     }
 }
