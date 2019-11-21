d8748bf8c30f7262fb2bb63689b0763e4125ba61
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-11-21 11:25:54 +1030

    sweet

--- I dont know/I dont know/Assets/Scripts/AdventureGame.cs
+++ I dont know/I dont know/Assets/Scripts/AdventureGame.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshPro;
-    
+    [SerializeField] private State startingState;
+
+    private State _state;
+
     void Start()
     {
-        textMeshPro.text = ("Hello there");
+        _state = startingState;
+        textMeshPro.text = _state.GetStateStory();
     }
 
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = _state.GetNextState();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _state = nextStates[0];
+        }
+
+        textMeshPro.text = _state.GetStateStory();
+    }
 }
