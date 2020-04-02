e75a1e806e7763faa1a2e97d571386c5c1493548
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-04-02 13:02:18 -0400

    ?

--- Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/DependInfo.cmake
+++ Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/DependInfo.cmake
@@ -4,14 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/ColorPixel.cpp" "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/ColorPixel.cpp.o"
-  "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/GrayPixel.cpp" "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/GrayPixel.cpp.o"
-  "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/Pgm.cpp" "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/Pgm.cpp.o"
-  "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/Ppm.cpp" "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/Ppm.cpp.o"
-  "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/Singleton.cpp" "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/Singleton.cpp.o"
-  "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/main.cpp" "/Users/gavintaylormcroy/Desktop/Work-Projects/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/main.cpp.o"
+  "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/ColorPixel.cpp" "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/ColorPixel.cpp.obj"
+  "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/GrayPixel.cpp" "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/GrayPixel.cpp.obj"
+  "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/Pgm.cpp" "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/Pgm.cpp.obj"
+  "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/Ppm.cpp" "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/Ppm.cpp.obj"
+  "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/Refactor.cpp" "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/Refactor.cpp.obj"
+  "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/main.cpp" "C:/Users/Gavin McRoy/Desktop/GitHubProjects/Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/main.cpp.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/cmake_clean.cmake
+++ Assignment #1/Assignment 1/cmake-build-debug/CMakeFiles/Assignment_1.dir/cmake_clean.cmake
@@ -1,12 +1,14 @@
 file(REMOVE_RECURSE
-  "Assignment_1"
+  "Assignment_1.exe"
+  "Assignment_1.exe.manifest"
   "Assignment_1.pdb"
-  "CMakeFiles/Assignment_1.dir/ColorPixel.cpp.o"
-  "CMakeFiles/Assignment_1.dir/GrayPixel.cpp.o"
-  "CMakeFiles/Assignment_1.dir/Pgm.cpp.o"
-  "CMakeFiles/Assignment_1.dir/Ppm.cpp.o"
-  "CMakeFiles/Assignment_1.dir/Singleton.cpp.o"
-  "CMakeFiles/Assignment_1.dir/main.cpp.o"
+  "CMakeFiles/Assignment_1.dir/ColorPixel.cpp.obj"
+  "CMakeFiles/Assignment_1.dir/GrayPixel.cpp.obj"
+  "CMakeFiles/Assignment_1.dir/Pgm.cpp.obj"
+  "CMakeFiles/Assignment_1.dir/Ppm.cpp.obj"
+  "CMakeFiles/Assignment_1.dir/Refactor.cpp.obj"
+  "CMakeFiles/Assignment_1.dir/main.cpp.obj"
+  "libAssignment_1.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
