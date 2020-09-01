ebd7df94cbc42e94d4651069758c2c8397110f5f
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-09-01 18:04:45 -0400

    words

---
+++ CPSC 2121/Lab02/Lab02-Source-Clion/cmake-build-debug/CMakeFiles/lk.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lk.dir/main.cpp.obj"
+  "CMakeFiles/lk.dir/stringset.cpp.obj"
+  "liblk.dll.a"
+  "lk.exe"
+  "lk.exe.manifest"
+  "lk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
