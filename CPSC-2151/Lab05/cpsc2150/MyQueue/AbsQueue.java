235ffe0010b12a6d8d6bab8803bd9bd7c36960c6
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-09-25 12:04:01 -0400

    extreme refactoring required

--- CPSC-2151/Lab05/cpsc2150/MyQueue/AbsQueue.java
+++ CPSC-2151/Lab05/cpsc2150/MyQueue/AbsQueue.java
@@ -2,9 +2,22 @@ package cpsc2150.MyQueue;
 
 public abstract class AbsQueue implements IQueue {
 
+    /*
+     * TODO FINISH IMPLEMENTATION
+     */
     @Override
     public String toString() {
-        return "AbsQueue{}";
+        String formattedQueue = "";
+        for (int i = 0; i < length(); i++) {
+            Integer tmp = dequeue();
+            if (i > 0) {
+                formattedQueue = formattedQueue.concat(", " + tmp);
+            } else {
+                formattedQueue = formattedQueue.concat(tmp + "");
+            }
+            enqueue(tmp);
+        }
+        return formattedQueue;
     }
 
 }
--- CPSC-2151/Lab05/cpsc2150/MyQueue/QueueApp.java
+++ CPSC-2151/Lab05/cpsc2150/MyQueue/QueueApp.java
@@ -7,70 +7,126 @@ public class QueueApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userAnswer;
-        IQueue q;
+        int userChoice = 0;
+        IQueue q = null;
 
         System.out.println("Do you want an array (Type array) or list implementation? (Type list) ");
         userAnswer = scanner.nextLine();
         userAnswer = userAnswer.toLowerCase();
-
-        //printMenu();
-
-        if (userAnswer.compareTo("list") == 0) {
-            System.out.println("Entered List");
-            Integer x = 42;
+        if (userAnswer.equals("list")) {
             q = new ListQueue();
-            q.enqueue(x);
-            x = 17;
-            q.enqueue(x);
-            x = 37;
-            q.enqueue(x);
-            x = 36;
-            q.enqueue(x);
-            x = 12;
-            q.enqueue(x);
-
-            /* TODO print all elements */
-            Integer tmp = q.endOfQueue();
-            q.printList();
-            System.out.println("--------INSERT---------");
-            q.insert(4, 3);
-            //System.out.println(tmp);
-            q.printList();
-            int myVal = q.remove(3);
-            System.out.println("--------REMOVE---------");
-            q.printList();
-            System.out.println("--------GET---------");
-            int myGet = q.get(3);
-            System.out.println("VALUE GOT = " + myGet);
-            q.printList();
-
-
-        } else if (userAnswer.compareTo("array") == 0) {
-            System.out.println("Entered array");
-            Integer x = 42;
+        } else if (userAnswer.equals("array")) {
             q = new ArrayQueue();
-            q.enqueue(x);
-            x = 17;
-            q.enqueue(x);
-            x = 37;
-            q.enqueue(x);
-            x = 36;
-            q.enqueue(x);
-            x = 12;
-            q.enqueue(x);
-
-            /* TODO print all elements I wasnt sure how to do it without
-             *  having a method inside the class */
-            q.peek();
-            q.printList();
-
         } else {
-            System.out.println("Invalid Input");
+            System.out.println("Invalid input");
+            System.exit(1);
+        }
+
+        while (true) {
+            printMenu();
+            if (scanner.hasNextInt()) {
+                userChoice = scanner.nextInt();
+            } else {
+                System.out.println("Invalid character");
+                System.exit(1);
+            }
+            switch (userChoice) {
+                /* Add to queue */
+                case 1:
+                    System.out.println("What number to add to the queue");
+                    int x = scanner.nextInt();
+                    q.enqueue(x);
+                    break;
+                /* Next number in the queue */
+                case 2:
+                    if (q.length() == 0) {
+                        System.out.println("Queue is empty!");
+                        break;
+                    }
+                    System.out.println("Next number in the queue is " + q.get(2));
+                    break;
+                /* Peek at first value in the queue */
+                case 3:
+                    if (q.length() == 0) {
+                        System.out.println("Queue is empty!");
+                        break;
+                    }
+                    System.out.println("Peek is " + q.peek());
+                    break;
+                /* Peek at the last value in the queue */
+                case 4:
+                    if (q.length() == 0) {
+                        System.out.println("Queue is empty!");
+                        break;
+                    }
+                    System.out.println("Peek at end " + q.endOfQueue());
+                    break;
+                /*TODO (Does not insert properly on empty list) Insert into a position in the queue */
+                case 5:
+                    System.out.println("What number to add to the queue? ");
+                    int valToAdd = scanner.nextInt();
+                    int positionToEnter;
+                    while (true) {
+                        System.out.println("What position to insert in? ");
+                        positionToEnter = scanner.nextInt();
+                        if (positionToEnter > q.length() || positionToEnter < 0) {
+                            break;
+                        } else {
+                            System.out.println("Enter valid position");
+                        }
+                    }
+                    q.insert(valToAdd, positionToEnter);
+                    break;
+                /* TODO Peek at a value at any position in the queue */
+                case 6:
+                    if (q.length() == 0) {
+                        System.out.println("Queue is empty!");
+                        break;
+                    }
+                    int positionToGet;
+                    while (true) {
+                        System.out.println("What position do you want from the queue");
+                        positionToGet = scanner.nextInt();
+                        if (positionToGet > q.length() || positionToGet < 0) {
+                            break;
+                        }else{
+                            System.out.println("Enter valid position");
+                        }
+                    }
+                    System.out.println("Value at position "+positionToGet+" is "+q.get(positionToGet));
+
+                    break;
+                /* TODO Remove a value from any position in the queue and return it */
+                case 7:
+                    if (q.length() == 0) {
+                        System.out.println("Queue is empty!");
+                        break;
+                    }
+                    int positionToGet;
+                    while (true) {
+                        System.out.println("What position do you want from the queue");
+                        positionToGet = scanner.nextInt();
+                        if (positionToGet > q.length() || positionToGet < 0) {
+                            break;
+                        }else{
+                            System.out.println("Enter valid position");
+                        }
+                    }
+                    break;
+                /* Exit */
+                case 8:
+                    System.out.println("Exiting...");
+                    System.exit(1);
+                    break;
+                default:
+            }
+            System.out.println("Queue is: \n" + q.toString());
         }
     }
 
     public static void printMenu() {
-        System.out.println("1. Add to the queue\n" +
+        System.out.println("Select an option\n" +
+                "1. Add to the queue\n" +
                 "2. Get the next number from the queue\n" +
                 "3. Peek at the first value in the queue\n" +
                 "4. Peek at the last value in the queue\n" +
@@ -80,13 +136,12 @@ public class QueueApp {
                 "8. Exit\n");
     }
 
-
     /**
-     * Questions:
-     * How are we supposed to implement the peek method if dequeue removes
-     * the integer from the front?
-     * I don't understand how we are supposed to implement secondary methods
-     * that use only primary methods to do things primary methods cannot do
+     * TODO:
+     * Finish Implementation of ToString();
+     * Finish Implementation of main()
+     * Finish new contract for new implemented methods inside of IQueue
+     *
      */
 }
 
