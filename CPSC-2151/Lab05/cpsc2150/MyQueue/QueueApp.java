50709c06c1ff2081a7e573b9edf5fb9abd86d0eb
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-09-25 17:00:14 -0400

    Refactoring

--- CPSC-2151/Lab05/cpsc2150/MyQueue/QueueApp.java
+++ CPSC-2151/Lab05/cpsc2150/MyQueue/QueueApp.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class QueueApp {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String userAnswer;
+        int usersImplementationChoice;
         int userChoice = 0;
         IQueue q = null;
 
-        System.out.println("Do you want an array (Type array) or list implementation? (Type list) ");
-        userAnswer = scanner.nextLine();
-        userAnswer = userAnswer.toLowerCase();
-        if (userAnswer.equals("list")) {
-            q = new ListQueue();
-        } else if (userAnswer.equals("array")) {
+        /* Input for which implementation the user wants */
+        System.out.println("Enter 1 for array implementation or 2 for List implementation ");
+        usersImplementationChoice = scanner.nextInt();
+
+        if (usersImplementationChoice == 1) {
             q = new ArrayQueue();
+        } else if (usersImplementationChoice == 2) {
+            q = new ListQueue();
         } else {
             System.out.println("Invalid input");
             System.exit(1);
@@ -32,93 +34,71 @@ public class QueueApp {
             }
             switch (userChoice) {
                 /* Add to queue */
-                case 1:
+                case 1: {
                     System.out.println("What number to add to the queue");
                     int x = scanner.nextInt();
                     q.enqueue(x);
                     break;
+                }
                 /* Next number in the queue */
-                case 2:
-                    if (q.length() == 0) {
-                        System.out.println("Queue is empty!");
+                case 2: {
+                    if (queueIsValidLength(q)) {
                         break;
                     }
+
                     System.out.println("Next number in the queue is " + q.get(2));
                     break;
+                }
                 /* Peek at first value in the queue */
-                case 3:
-                    if (q.length() == 0) {
-                        System.out.println("Queue is empty!");
+                case 3: {
+                    if (queueIsValidLength(q)) {
                         break;
                     }
+
                     System.out.println("Peek is " + q.peek());
                     break;
+                }
                 /* Peek at the last value in the queue */
-                case 4:
-                    if (q.length() == 0) {
-                        System.out.println("Queue is empty!");
+                case 4: {
+                    if (queueIsValidLength(q)) {
                         break;
                     }
                     System.out.println("Peek at end " + q.endOfQueue());
                     break;
+                }
                 /*TODO (Does not insert properly on empty list) Insert into a position in the queue */
-                case 5:
+                case 5: {
                     System.out.println("What number to add to the queue? ");
                     int valToAdd = scanner.nextInt();
-                    int positionToEnter;
-                    while (true) {
-                        System.out.println("What position to insert in? ");
-                        positionToEnter = scanner.nextInt();
-                        if (positionToEnter > q.length() || positionToEnter < 0) {
-                            break;
-                        } else {
-                            System.out.println("Enter valid position");
-                        }
-                    }
+                    int positionToEnter = readDesiredPosition(q);
                     q.insert(valToAdd, positionToEnter);
                     break;
+                }
                 /* TODO Peek at a value at any position in the queue */
-                case 6:
-                    if (q.length() == 0) {
-                        System.out.println("Queue is empty!");
+                case 6: {
+                    if (queueIsValidLength(q)) {
                         break;
                     }
-                    int positionToGet;
-                    while (true) {
-                        System.out.println("What position do you want from the queue");
-                        positionToGet = scanner.nextInt();
-                        if (positionToGet > q.length() || positionToGet < 0) {
-                            break;
-                        }else{
-                            System.out.println("Enter valid position");
-                        }
-                    }
-                    System.out.println("Value at position "+positionToGet+" is "+q.get(positionToGet));
-
+                    int positionToGet = readDesiredPosition(q);
+                    System.out.println("Value at position " + positionToGet + " is " + q.get(positionToGet));
                     break;
+                }
                 /* TODO Remove a value from any position in the queue and return it */
-                case 7:
-                    if (q.length() == 0) {
-                        System.out.println("Queue is empty!");
+                case 7: {
+                    if (queueIsValidLength(q)) {
                         break;
                     }
-                    int positionToGet;
-                    while (true) {
-                        System.out.println("What position do you want from the queue");
-                        positionToGet = scanner.nextInt();
-                        if (positionToGet > q.length() || positionToGet < 0) {
-                            break;
-                        }else{
-                            System.out.println("Enter valid position");
-                        }
-                    }
+                    int pos = readDesiredPosition(q);
                     break;
+                }
                 /* Exit */
                 case 8:
                     System.out.println("Exiting...");
                     System.exit(1);
                     break;
                 default:
+                    System.out.println("Not a valid option! ");
+                    break;
             }
             System.out.println("Queue is: \n" + q.toString());
         }
@@ -134,6 +114,34 @@ public class QueueApp {
                 "6. Peek at a value in any position in the queue\n" +
                 "7. Remove a value from any position in the queue and return it\n" +
                 "8. Exit\n");
+    }
+
+    /**
+     * TODO Implement Contract
+     */
+    public static boolean queueIsValidLength(IQueue q) {
+        if (q.length() == 0) {
+            System.out.println("Queue is empty!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * TODO Implement Contract
+     */
+    public static int readDesiredPosition(IQueue q) {
+        int positionToGet;
+        while (true) {
+            System.out.println("What position do you want from the queue");
+            positionToGet = scanner.nextInt();
+            if (positionToGet > q.length() || positionToGet < 0) {
+                break;
+            } else {
+                System.out.println("Enter valid position");
+            }
+        }
+        return positionToGet;
     }
 
     /**
