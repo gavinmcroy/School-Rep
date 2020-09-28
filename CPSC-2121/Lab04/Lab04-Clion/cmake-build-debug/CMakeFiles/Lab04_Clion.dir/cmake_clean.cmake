302754162f2e8c36fc9805e1d3ced0309b7f40cb
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-09-28 09:13:27 -0400

    works?

---
+++ CPSC-2121/Lab04/Lab04-Clion/cmake-build-debug/CMakeFiles/Lab04_Clion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab04_Clion.dir/bst.cpp.obj"
+  "Lab04_Clion.exe"
+  "Lab04_Clion.exe.manifest"
+  "Lab04_Clion.pdb"
+  "libLab04_Clion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab04_Clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
