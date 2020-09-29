dde47e89312f6178ae3141d1a39fb26642de9f94
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2020-09-29 09:40:41 -0400

    Cleaning it up

--- CPSC-2120/Project-1/Utils.cpp
+++ CPSC-2120/Project-1/Utils.cpp
@@ -80,174 +80,146 @@ void processKeystrokes() {
     cout << color_white;
 }
 
-/* This shows how to use some of the starter code above */
 int main() {
+
     StoredWebPages webPages;
     StoredWords storedWords;
-
     StringIntMap webPageIntMap;
     StringIntMap wordIntMap;
-    StringIntMap * wordOnPage = new StringIntMap;
-
-    cout << color_green << "Reading input..." << endl;
-    const char *filename = "/Users/gavintaylormcroy/Desktop/webpages.txt";
-    istringstream webfile(readWebpagesFast(filename));
-
+    auto *wordOnPage = new StringIntMap;
     std::vector<StoredWebPages::Webpage> pages = webPages.getWebPages();
     std::vector<StoredWords::Word> everyDistinctWordVec = storedWords.getWords();
+    const char *filename = "/Users/gavintaylormcroy/Desktop/webpages.txt";
+    string readInData;
 
-    /* I could insert these string values of page into the hash table
-     * then when I am checking all my hyper links I can call find. If I find it
-     * I can check its location, and store the corresponding location inside of
-     * my vector<int> */
-
-    int totalWebPages = 0;
-    int totalDistinctWords = 0;
-    string data;
+    cout << color_green << "Reading input..." << endl;
+    istringstream webFile(readWebpagesFast(filename));
 
     /* Insert all PAGES and DISTINCT words into separate hash table */
-    while (webfile >> data) {
-        if (data == "PAGE") {
-            webfile >> data;
-            webPageIntMap.insert(data, totalWebPages);
-            totalWebPages++;
-        } else if (data == "LINK ") {
-            string hyperLinkTMP;
-            /* This is catching the hyper link and just trashing it */
-            webfile >> hyperLinkTMP;
-        } else {
-            /* Store all the DISTINCT words in a map/hash table THAT containing
-             * the words INDEX/POS for the words INSIDE the vector located in StoredWords */
-            if (!wordIntMap.find(data)) {
-                wordIntMap.insert(data, totalDistinctWords);
-                /* This is adding the words to the vector */
-                everyDistinctWordVec.emplace_back(data);
-                totalDistinctWords++;
+    {
+        int totalWebPages = 0;
+        int totalDistinctWords = 0;
+        while (webFile >> readInData) {
+            if (readInData == "PAGE") {
+                webFile >> readInData;
+                webPageIntMap.insert(readInData, totalWebPages);
+                totalWebPages++;
+            } else if (readInData == "LINK ") {
+                /* This is catching the hyper link and just trashing it */
+                webFile >> readInData;
+            } else {
+                /* Store all the DISTINCT words in a map/hash table THAT containing
+                 * the words INDEX/POS for the words INSIDE the vector located in StoredWords */
+                if (!wordIntMap.find(readInData)) {
+                    wordIntMap.insert(readInData, totalDistinctWords);
+                    /* This is adding the words to the vector */
+                    everyDistinctWordVec.emplace_back(readInData);
+                    totalDistinctWords++;
+                }
             }
         }
     }
-//    cout << words.size() << endl;
-//    cout << totalDistinctWords << endl;
     cout << "Indexing..." << endl;
 
-
     /* Reset the file being read */
-    webfile.clear();
-    webfile.seekg(0);
+    webFile.clear();
+    webFile.seekg(0);
 
     /* 26,881 Pages, 184,408 Hyperlinks, 18,896,392 words */
 
-    /* COUNTERS */
-    string readInValue;
-    string url;
-    int numLinks = 0;
-    int numWords = 0;
-    vector<int> links;
-    vector<string> wordsOccurringInPage;
-    /* COUNTERS */
-
-    /* Parameters */
-    bool first = true;
-    string hyperLink;
-    int tmpUniqueWords = 0;
-    /* Parameters */
-
     /* This builds our webpage vector with all appropriate info */
-    while (webfile >> readInValue) {
-        if (readInValue == "PAGE") {
-            if (!first) {
-                /* Reset all the counters by adding the WebPage + all its data */
-                pages.emplace_back(url, numLinks, numWords, links, wordsOccurringInPage);
-                wordOnPage->~StringIntMap();
-                wordOnPage = new StringIntMap();
-                wordsOccurringInPage.clear();
-                numLinks = 0;
-                numWords = 0;
-                tmpUniqueWords = 0;
-                /* Reset all the counters */
+    {
+        string url;
+        int numLinksOnPageCounter = 0;
+        int numWordsOnPageCounter = 0;
+        int uniqueWordsOnPageCounter = 0;
+        bool firstIteration = true;
+        vector<int> links;
+        vector<string> wordsOccurringInPage;
+
+        while (webFile >> readInData) {
+            if (readInData == "PAGE") {
+                if (!firstIteration) {
+                    /* Reset all the counters by adding the WebPage + all its data */
+                    pages.emplace_back(url, numLinksOnPageCounter, numWordsOnPageCounter, links, wordsOccurringInPage);
+                    wordOnPage->~StringIntMap();
+                    wordOnPage = new StringIntMap();
+                    wordsOccurringInPage.clear();
+                    numLinksOnPageCounter = 0;
+                    numWordsOnPageCounter = 0;
+                    uniqueWordsOnPageCounter = 0;
+                    /* Reset all the counters */
+                }
+                firstIteration = false;
+                /* Read in because you want the PAGE then the url next to it */
+                webFile >> url;
             }
-            first = false;
-            /* Read in because you want the PAGE then the url next to it */
-            webfile >> url;
-        } /* Reads a hyperLink, calculates the index at which the hyperLink would exist inside inside the StringToIntMap
-           * hash table. It then determines if the hyperLink in question exists inside the hash table. If it exists
-           * inside the hash table, a reference value is given that corresponds to the index at which this link
-           * would exist inside the Vector of WebPages. Reference value is given on first file read */
-        else if (readInValue == "LINK") {
-            /* Read in because you want LINK then the url next to it */
-            webfile >> hyperLink;
-            /* If the link is not a dead end */
-            if (webPageIntMap.find(hyperLink)) {
-                int hash = webPageIntMap[hyperLink];
-                links.push_back(hash);
-                numLinks++;
+                /* Reads a hyperLink, calculates the index at which the hyperLink would exist inside inside the StringToIntMap
+                * hash table. It then determines if the hyperLink in question exists inside the hash table. If it exists
+                * inside the hash table, a reference value is given that corresponds to the index at which this link
+                * would exist inside the Vector of WebPages. Reference value is given on first file read */
+            else if (readInData == "LINK") {
+                /* Read in because you want LINK then the url next to it */
+                webFile >> readInData;
+                /* If the link is not a dead end */
+                if (webPageIntMap.find(readInData)) {
+                    int location = webPageIntMap[readInData];
+                    links.push_back(location);
+                    numLinksOnPageCounter++;
+                }
+            } /* All other words are processed */
+            else {
+                /* These are the words that OCCUR within a particular page */
+                wordsOccurringInPage.push_back(readInData);
+                numWordsOnPageCounter++;
+                /* This is adding only non duplicate words from a particular page*/
+                if (!wordOnPage->find(readInData)) {
+                    /* A lot is happening here. We are taking the index at which the page exists
+                    * in the page structure. Then getting the index at which the word exists in the word structure.
+                    * then we are accessing the index at which the word exists, adding the index at which the link exists
+                    * to the word struct. Then while inside the word struct we are incrementing num pages to signify
+                    * how many pages the word is on*/
+                    wordOnPage->insert(readInData, uniqueWordsOnPageCounter);
+                    int linkIndex = webPageIntMap[url];
+                    int wordIndex = wordIntMap[readInData];
+                    everyDistinctWordVec.at(wordIndex).pages.push_back(linkIndex);
+                    everyDistinctWordVec.at(wordIndex).numPages++;
+                    uniqueWordsOnPageCounter++;
+                }
             }
-        } /* Regular Words are read in and added to the wordsOccurring vector*/
-        else {
-            /* These are the words that OCCUR within the scope of a page */
-
-            wordsOccurringInPage.push_back(readInValue);
-            numWords++;
-
-            /* A lot is happening here. We are taking the index at which the page exists
-             * in the page structure. Then getting the index at which the word exists in the word structure.
-             * then we are accessing the index at which the word exists, adding the index at which the link exists
-             * to the word struct. Then while inside the word struct we are incrementing num pages to signify
-             * how many pages the word is on*/
-
-            /* This is adding only non duplicate words from a particular page*/
-            if (!wordOnPage->find(readInValue)) {
-                wordOnPage->insert(readInValue, tmpUniqueWords);
-                int linkIndex = webPageIntMap[url];
-                int wordIndex = wordIntMap[readInValue];
-                everyDistinctWordVec.at(wordIndex).pages.push_back(linkIndex);
-                everyDistinctWordVec.at(wordIndex).numPages++;
-                tmpUniqueWords++;
-                /* This is adding the words to the vector */
-                //words.emplace_back(data);
-                //totalDistinctWords++;
-            }
-
-
-            /* TODO THIS IS counting duplicates */
-//            int linkIndex = webPageIntMap[url];
-//            int wordIndex = wordIntMap[readInValue];
-//            words.at(wordIndex).pages.push_back(linkIndex);
-//            words.at(wordIndex).numPages++;
         }
     }
-    delete wordOnPage;
 
+    /* TEST SEARCH */
     int myLookUp = wordIntMap["dabo"];
     cout << everyDistinctWordVec.at(myLookUp).text << endl;
-    // cout << "Frequency " << myLookUp << endl;
     cout << "Total Pages " << everyDistinctWordVec.at(myLookUp).numPages << endl;
+
     myLookUp = wordIntMap["football"];
     cout << everyDistinctWordVec.at(myLookUp).text << endl;
-    //cout << "Frequency " << myLookUp << endl;
     cout << "Total Pages " << everyDistinctWordVec.at(myLookUp).numPages << endl;
+
     myLookUp = wordIntMap["pancakes"];
     cout << everyDistinctWordVec.at(myLookUp).text << endl;
-    //cout << "Frequency " << words.at(myLookUp). << endl;
     cout << "Total Pages " << everyDistinctWordVec.at(myLookUp).numPages << endl;
 
-    for (int i = 0; i < everyDistinctWordVec.at(myLookUp).pages.size(); i++) {
-        cout << everyDistinctWordVec.at(myLookUp).pages.at(i) << endl;
-    }
+    myLookUp = wordIntMap["pancake"];
+    cout << everyDistinctWordVec.at(myLookUp).text << endl;
+    cout << "Total Pages " << everyDistinctWordVec.at(myLookUp).numPages << endl;
+    /* TEST SEARCH */
 
-    /* DEBUG */
-//    cout << pages.at(0).url << endl;
-//    cout << pages.at(0).numLinks << endl;
-//
-//    cout << pages.at(0).words.size() << endl;
-//    cout << pages.at(0).words.at(0) << endl;
-    /* DEBUG */
+    delete wordOnPage;
 
-    // Enter loop to ask for query
+    /* Enter loop to ask for query */
     processKeystrokes();
 
     return 0;
 }
+
+/** Note I could insert these string values of page into the hash table
+ * then when I am checking all my hyper links I can call find. If I find it
+ * I can check its location, and store the corresponding location inside of
+ * my vector<int> */
 
 /** REFERENCES */
 //while (webfile >> s) {
@@ -303,3 +275,16 @@ int main() {
 //            /* This is all the words in the entire file */
 //        }
 //    }
+
+/* DEBUG */
+//    for (int i = 0; i < everyDistinctWordVec.at(myLookUp).pages.size(); i++) {
+//        cout << everyDistinctWordVec.at(myLookUp).pages.at(i) << endl;
+//    }
+
+
+//    cout << pages.at(0).url << endl;
+//    cout << pages.at(0).numLinks << endl;
+//
+//    cout << pages.at(0).words.size() << endl;
+//    cout << pages.at(0).words.at(0) << endl;
+/* DEBUG */
