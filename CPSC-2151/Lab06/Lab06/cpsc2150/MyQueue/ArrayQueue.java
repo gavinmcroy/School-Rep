f11b22dd8621fd489bd076219864335288f510eb
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-10-02 09:19:27 -0400

    uh

---
+++ CPSC-2151/Lab06/Lab06/cpsc2150/MyQueue/ArrayQueue.java
@@ -0,0 +1,122 @@
+package cpsc2150.MyQueue;
+
+import java.lang.reflect.Array;
+
+public class ArrayQueue<T> extends AbsQueue implements IQueue<T> {
+
+    /**
+     * where the data is stored. myQ[0] is the front of the queue
+     */
+    private T[] myQ;
+//
+
+
+    /**
+     * tracks how many items in the queue
+     * also used to find the end of the queue
+     *
+     * @invariant 0 <= myLength < MAX_LENGTH
+     * @correspondence total_element = myLength
+     */
+    private int myLength = 0;
+
+    /**
+     * Initializes integer array of size MAX_LENGTH and initializes every
+     * element in the array to 0
+     *
+     * @pre none
+     * @post myQ == 0
+     */
+    ArrayQueue() {
+        myQ = new T[MAX_LENGTH];
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            myQ[i] = 0;
+        }
+//        @SuppressWarnings("unchecked")
+//        final T[] a = (T[]) Array.newInstance(c, MAX_LENGTH);
+//        this.a = a;
+
+    }
+
+    /**
+     * Queues a integer into the array by adding it to the back of the array
+     *
+     * @param x the integer to enqueue into the array
+     */
+    @Override
+    public void enqueue(T x) {
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            if (myQ[i] != 0 && i != 0) {
+                int val = myQ[i];
+                myQ[i - 1] = val;
+            }
+        }
+        myQ[MAX_LENGTH - 1] = x;
+        myLength++;
+    }
+
+    /**
+     * removes and returns the Integer at the front of the queue
+     *
+     * @return the variable at the front of the queue, 0 if a variable is not found
+     */
+    @Override
+    public Integer dequeue() {
+        int firstInteger = 0;
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            if (myQ[i] != 0) {
+                firstInteger = i;
+                break;
+            }
+        }
+        if (myQ[firstInteger] != 0) {
+            int tmp = myQ[firstInteger];
+            myQ[firstInteger] = 0;
+            myLength--;
+            return tmp;
+        }
+        return 0;
+    }
+
+    /**
+     * Gets the current length of the queue
+     *
+     * @return the current length of the queue
+     */
+    @Override
+    public int length() {
+        return myLength;
+    }
+
+    /**
+     * Clears the queue by setting every element back equal to 0.
+     * If the queue is empty it does nothing
+     */
+    @Override
+    public void clear() {
+        if (myLength == 0) {
+            return;
+        }
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            myQ[i] = 0;
+        }
+        myLength = 0;
+    }
+
+    /* The lab asks for the list to be printed */
+
+    /**
+     * Prints the current elements inside the queue
+     *
+     * @pre none
+     * @post none
+     */
+//    public void printList() {
+//        for (Integer integer : myQ) {
+//            if (integer == 0) {
+//                continue;
+//            }
+//            System.out.println(integer);
+//        }
+//    }
+}
