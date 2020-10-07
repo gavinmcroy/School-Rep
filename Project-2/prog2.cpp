cf5c24df35fc114b1208c00c4a06cfb3af24e1e1
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2020-10-07 15:24:09 -0400

    first and second solution

--- Project-2/prog2.cpp
+++ Project-2/prog2.cpp
@@ -11,7 +11,7 @@ struct Node {
 };
 
 Node **T; // array of Node *s for hash table
-int table_size = 10000;  
+int table_size = 10000;
 
 // check out this awesome hash function!
 int hash(int x)
@@ -21,14 +21,14 @@ int hash(int x)
 
 bool find(int x)
 {
-  for (Node *n = T[hash(x)]; n != NULL; n = n->next)
+  for (Node *n = T[::hash(x)]; n != NULL; n = n->next)
     if (n->val == x) return true;
   return false;
 }
 
 void insert(int x)
 {
-  int index = hash(x);
+  int index = ::hash(x);
   T[index] = new Node(x, T[index]);
 }
 
