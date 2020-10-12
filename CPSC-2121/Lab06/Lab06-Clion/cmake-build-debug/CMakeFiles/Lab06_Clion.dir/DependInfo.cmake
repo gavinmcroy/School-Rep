fc28057881201a4a7d7b46c79f4f107c3f2fd8fb
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-10-11 21:14:55 -0400

    kaboom

--- CPSC-2121/Lab06/Lab06-Clion/cmake-build-debug/CMakeFiles/Lab06_Clion.dir/DependInfo.cmake
+++ CPSC-2121/Lab06/Lab06-Clion/cmake-build-debug/CMakeFiles/Lab06_Clion.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/gavintaylormcroy/Desktop/School-Rep/CPSC-2121/Lab06/Lab06-Clion/starter_code.cpp" "/Users/gavintaylormcroy/Desktop/School-Rep/CPSC-2121/Lab06/Lab06-Clion/cmake-build-debug/CMakeFiles/Lab06_Clion.dir/starter_code.cpp.o"
+  "C:/Users/Gavin McRoy/Desktop/School-Rep/CPSC-2121/Lab06/Lab06-Clion/starter_code.cpp" "C:/Users/Gavin McRoy/Desktop/School-Rep/CPSC-2121/Lab06/Lab06-Clion/cmake-build-debug/CMakeFiles/Lab06_Clion.dir/starter_code.cpp.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- CPSC-2121/Lab06/Lab06-Clion/cmake-build-debug/CMakeFiles/Lab06_Clion.dir/cmake_clean.cmake
+++ CPSC-2121/Lab06/Lab06-Clion/cmake-build-debug/CMakeFiles/Lab06_Clion.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Lab06_Clion.dir/starter_code.cpp.o"
-  "Lab06_Clion"
+  "CMakeFiles/Lab06_Clion.dir/starter_code.cpp.obj"
+  "Lab06_Clion.exe"
+  "Lab06_Clion.exe.manifest"
   "Lab06_Clion.pdb"
+  "libLab06_Clion.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
