9daca390cf9c2337b5916615f46de5606c598f4f
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2020-10-22 15:25:57 -0400

    .

---
+++ CPSC-2120/Exam-1/2121-Exam1-Clion/Prob3/cmake-build-debug/CMakeFiles/Prob3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prob3.dir/prob3_starter.cpp.o"
+  "Prob3"
+  "Prob3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prob3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
