fad524bc2811bf2c8327bbc5c50174fbe3ff44e0
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-11-03 13:28:01 -0500

    Update main.cpp

--- CPSC-2121/Lab08/main.cpp
+++ CPSC-2121/Lab08/main.cpp
@@ -5,5 +5,6 @@ int main() {
     TSP tsp("tsp_points.txt");
     std::cout << "Optimal Tour: " << tsp.calculateOptimalTour() << std::endl;
     tsp.outputTour();
+    std::cout<<"Optimal Tour is in Output.txt"<<std::endl;
     return 0;
 }
