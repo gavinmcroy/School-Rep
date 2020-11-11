88fdd69cca7666f23ee773674583762999a3c312
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2020-11-11 13:07:25 -0500

    Huge Win

---
+++ Pls-Work/cmake-build-debug/CMakeFiles/Pls_Work.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gavin McRoy/Desktop/Pls-Work/Jugs.cpp" "C:/Users/Gavin McRoy/Desktop/Pls-Work/cmake-build-debug/CMakeFiles/Pls_Work.dir/Jugs.cpp.obj"
+  "C:/Users/Gavin McRoy/Desktop/Pls-Work/reference.cpp" "C:/Users/Gavin McRoy/Desktop/Pls-Work/cmake-build-debug/CMakeFiles/Pls_Work.dir/reference.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
