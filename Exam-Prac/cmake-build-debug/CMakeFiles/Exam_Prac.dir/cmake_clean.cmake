8ae9b6d2c56c772dc4c2b2b278b32492ef8d1d06
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2020-12-09 14:09:45 -0500

    exam practice

---
+++ Exam-Prac/cmake-build-debug/CMakeFiles/Exam_Prac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exam_Prac.dir/main.cpp.o"
+  "Exam_Prac"
+  "Exam_Prac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exam_Prac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
