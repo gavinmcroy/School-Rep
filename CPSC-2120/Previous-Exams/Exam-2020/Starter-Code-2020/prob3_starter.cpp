af8a8f48b2c3ae2f8d353b3717a081a30df174fa
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2021-01-09 21:22:36 -0500

    prayers

--- CPSC-2120/Previous-Exams/Exam-2020/Starter-Code-2020/prob3_starter.cpp
+++
@@ -1,5 +0,0 @@
-// Guaranteed to output a hash value in the range 0 ... 999,999
-unsigned int myhash(int x)
-{
-  return ((unsigned)x * 123147 + 293871) % 100 + 900000;
-}
--- CPSC-2121/Lab02/Lab02-Source-Clion/cmake-build-debug/CMakeFiles/lk.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lk.
-# This may be replaced when dependencies are built.
