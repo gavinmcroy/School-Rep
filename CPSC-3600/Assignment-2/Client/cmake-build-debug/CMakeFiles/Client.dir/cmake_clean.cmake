94a394be4060172e08c276679bdac286ac5c9f92
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2021-03-11 07:39:12 -0500

    whyItWorksOnlyOnUnix

---
+++ CPSC-3600/Assignment-2/Client/cmake-build-debug/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/DieWithMessage.c.obj"
+  "CMakeFiles/Client.dir/TCPClientUtility.c.obj"
+  "CMakeFiles/Client.dir/TCPEchoClient.c.obj"
+  "Client.exe"
+  "Client.exe.manifest"
+  "Client.pdb"
+  "libClient.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
