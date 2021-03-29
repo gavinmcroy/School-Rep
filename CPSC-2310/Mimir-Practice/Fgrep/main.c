85c3121fd455bd91a44d3e72edb0cd2559290d87
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2021-03-29 10:48:26 -0400

    working assignment

--- CPSC-2310/Mimir-Practice/Fgrep/main.c
+++ CPSC-2310/Mimir-Practice/Fgrep/main.c
@@ -7,6 +7,10 @@ void search(char *fileName, FILE *input, char *searchString);
 
 int main(int argc, char *argv[]) {
     const int MIN_COMMANDS = 3;
+    if (argc < MIN_COMMANDS) {
+        printf("Usage: executable string file ...\n");
+        exit(0);
+    }
     int loopIterations = argc - MIN_COMMANDS;
     char *searchString = argv[1];
     int additionalCommands = 0;
@@ -14,35 +18,15 @@ int main(int argc, char *argv[]) {
         FILE *file = fopen(argv[MIN_COMMANDS + additionalCommands - 1], "r");
         if (!file) {
             perror(argv[MIN_COMMANDS + additionalCommands - 1]);
+        } else {
+            search(argv[MIN_COMMANDS + additionalCommands - 1], file, searchString);
+            printf("\n");
+            fclose(file);
         }
-        search(argv[MIN_COMMANDS + additionalCommands - 1], file, searchString);
-        printf("\n");
-        fclose(file);
+
         additionalCommands++;
         loopIterations--;
     }
-
-
-
-
-
-//    if (argc == 3) {
-//        FILE *file = fopen(argv[argc - 1], "r");
-//        if (!file) {
-//            perror(argv[argc - 1]);
-//        }
-//        search(argv[argc - 1], file, argv[argc - 2]);
-//        fclose(file);
-//    } else if (argc == 4) {
-//        FILE *file = fopen(argv[argc - 2], "r");
-//        FILE *file2 = fopen(argv[argc - 1], "r");
-//        search(argv[argc - 2], file, argv[argc - 3]);
-//        printf("\n");
-//        search(argv[argc - 1], file2, argv[argc - 3]);
-//        fclose(file);
-//        fclose(file2);
-//    }
-    //---Read file contents
     return 0;
 }
 
