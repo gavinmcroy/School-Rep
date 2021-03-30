dc513c499d65f24c7044a83555a4d78405af8b10
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2021-03-30 15:51:44 -0400

    organization

---
+++ CPSC-2310/Fgrep/Fgrep/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+void search(char *fileName, FILE *input, char *searchString);
+int main(int argc, char *argv[]) {
+    const int MIN_COMMANDS = 3;
+    if (argc < MIN_COMMANDS) {
+        printf("Usage: executable string file ...\n");
+        exit(0);
+    }
+    int loopIterations = argc - MIN_COMMANDS;
+    char *searchString = argv[1];
+    int additionalCommands = 0;
+    while (loopIterations >= 0) {
+        FILE *file = fopen(argv[MIN_COMMANDS + additionalCommands - 1], "r");
+        if (!file) {
+            perror(argv[MIN_COMMANDS + additionalCommands - 1]);
+        } else {
+            search(argv[MIN_COMMANDS + additionalCommands - 1], file, searchString);
+            printf("\n");
+            fclose(file);
+        }
+
+        additionalCommands++;
+        loopIterations--;
+    }
+    return 0;
+}
+void search(char *fileName, FILE *input, char *searchString) {
+    int bufferSize = 256;
+    char *buffer = (char *) malloc(sizeof(char) * bufferSize);
+    while (fgets(buffer, 260, input) != NULL) {
+        if (strstr(buffer, searchString) != NULL) {
+            printf("%s:%s", fileName, buffer);
+        }
+    }
+    free(buffer);
+}
