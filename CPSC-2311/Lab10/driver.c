393ccdbc9a220109b8b271606461631ecf249926
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2021-04-19 15:40:38 -0400

    simpleton

---
+++ CPSC-2311/Lab10/driver.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(){
+    int min = -5;
+    int max = 1;
+    
+    for(int i = min; i<=max; i++){
+        printf("%d",i);
+    }
+    printf("\n");
+    return 0;
+}
