d6435dd3d6f896ec96ddd5b17bf6c510e1734de2
gavgavmcroy <41348589+gavgavmcroy@users.noreply.github.com>
2021-05-17 11:20:04 -0400

    decent stuff

---
+++ Interesting-Code/Make_Heap/cmake-build-debug/CMakeFiles/Make_Heap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Make_Heap.dir/main.cpp.obj"
+  "Make_Heap.exe"
+  "Make_Heap.exe.manifest"
+  "Make_Heap.pdb"
+  "libMake_Heap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Make_Heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
