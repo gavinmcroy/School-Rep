18161c45e81f6439b7207d005a2d069f27851047
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2021-06-03 20:37:55 -0400

    some shit

---
+++ Leet-Code/Add-Two-Numbers/cmake-build-debug/CMakeFiles/Add_Two_Numbers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Add_Two_Numbers"
+  "Add_Two_Numbers.pdb"
+  "CMakeFiles/Add_Two_Numbers.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Add_Two_Numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
