4595f1cddb45f447a0b8a4e81b9131debf006e4c
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2021-09-08 19:29:05 -0400

    arrow keys work

--- CPSC-4040/Image/main.cpp
+++ CPSC-4040/Image/main.cpp
@@ -25,6 +25,8 @@ void reshapeFunction(int x, int y);
 
 void keyboardEvent(unsigned char key, int x, int y);
 
+void specialKeyboardEvent(int key, int x, int y);
+
 void loadImage(const std::string &inputFile);
 
 void writeImage(const std::string &outputFile);
@@ -35,9 +37,8 @@ int getImagePixelTypeForGL();
 
 int state = 0;
 //unsigned char *pixels = nullptr;
-std::vector<unsigned char*> images;
+std::vector<unsigned char *> images;
 std::vector<ImageSpec> specs;
-bool displayOn = true;
 //ImageSpec spec;
 
 int main(int argc, char *argv[]) {
@@ -48,12 +49,17 @@ int main(int argc, char *argv[]) {
      * Implement Multiple Image Input/Output */
 
     if (argc > 1) {
-        loadImage(argv[1]);
+        for (int i = 1; argv[i] != nullptr; i++) {
+            loadImage(argv[i]);
+        }
     }
 
     openGLSetup(argc, argv);
-    for(int i = 0; i < images.size(); i++);
-    delete pixels;
+
+    /* Clear memory inside the vector */
+    for (auto &image : images) {
+        delete image;
+    }
     return 0;
 }
 
@@ -61,15 +67,16 @@ int main(int argc, char *argv[]) {
 void openGLSetup(int argc, char *argv[]) {
     glutInit(&argc, argv);
     glutInitWindowPosition(0, 0);
-    if (!pixels) {
+    if (images.empty()) {
         glutInitWindowSize(900, 900);
     } else {
-        glutInitWindowSize(spec.width, spec.height);
+        glutInitWindowSize(specs[state].width, specs[state].height);
     }
     glutCreateWindow("My amazing window");
     glutDisplayFunc(displayFunction);
     glutReshapeFunc(reshapeFunction);
     glutKeyboardFunc(keyboardEvent);
+    glutSpecialFunc(specialKeyboardEvent);
     glutMainLoop();
 }
 
@@ -78,8 +85,9 @@ void displayFunction() {
     glClearColor(0, 0, 0, 0);
     glRasterPos2i(0, 0);
     glWindowPos2i(0, 0);
-    if (pixels != nullptr && displayOn) {
-        glDrawPixels(spec.width, spec.height, getImagePixelTypeForGL(), GL_UNSIGNED_BYTE, pixels);
+    if (!images.empty()) {
+        glDrawPixels(specs[state].width, specs[state].height, getImagePixelTypeForGL(), GL_UNSIGNED_BYTE,
+                     images[state]);
         glFlush();
     }
     glClear(GL_COLOR_BUFFER_BIT);
@@ -101,8 +109,6 @@ void keyboardEvent(unsigned char key, int x, int y) {
     switch (key) {
         case 'k': {
             std::cout << "Image cleared from buffer" << std::endl;
-            delete pixels;
-            pixels = nullptr;
             displayFunction();
             glFlush();
             break;
@@ -119,7 +125,8 @@ void keyboardEvent(unsigned char key, int x, int y) {
             std::cout << "i key pressed " << std::endl;
             invertImage();
             displayFunction();
-            glDrawPixels(spec.width, spec.height, getImagePixelTypeForGL(), GL_UNSIGNED_BYTE, pixels);
+            glDrawPixels(specs[state].width, specs[state].height, getImagePixelTypeForGL(), GL_UNSIGNED_BYTE,
+                         images[state]);
             glFlush();
             break;
         }
@@ -139,35 +146,77 @@ void keyboardEvent(unsigned char key, int x, int y) {
     }
 }
 
+void specialKeyboardEvent(int key, int x, int y) {
+    switch (key) {
+        case GLUT_KEY_LEFT: {
+            std::cout << "Left " << state << std::endl;
+            if (state == 0) {
+                state = 0;
+            } else {
+                state--;
+            }
+            displayFunction();
+            glDrawPixels(specs[state].width, specs[state].height, getImagePixelTypeForGL(), GL_UNSIGNED_BYTE,
+                         images[state]);
+            glFlush();
+            break;
+        }
+        case GLUT_KEY_RIGHT: {
+            std::cout << "Right " << state << std::endl;
+            if (state == images.size() - 1) {
+                state = 0;
+            } else {
+                state++;
+            }
+            displayFunction();
+            glDrawPixels(specs[state].width, specs[state].height, getImagePixelTypeForGL(), GL_UNSIGNED_BYTE,
+                         images[state]);
+            glFlush();
+            break;
+        }
+        default:
+            break;
+    }
+}
+
 /* Loads the image and allocates required memory. Also prints the image statistics
  * and assigns the global value spec to hold all required image info */
 void loadImage(const std::string &inputFile) {
+    unsigned char *pixels = nullptr;
     auto input = ImageInput::open(inputFile);
     if (!input) {
         std::cerr << "Error with file. " << geterror();
         exit(-1);
     }
 
-    spec = input->spec();
+    specs.push_back(input->spec());
+    int pos = specs.size() - 1;
+
     /* TODO Special case for gray scale images */
     if (getImagePixelTypeForGL() == GL_LUMINANCE) {
         int RGB = 3;
-        pixels = new unsigned char[spec.width * spec.height * RGB];
-        input->read_image(TypeDesc::UINT8, pixels);
+        auto *pixelsTemp = new unsigned char[specs[pos].width * specs[pos].height];
+        input->read_image(TypeDesc::UINT8, pixelsTemp);
+        pixels = new unsigned char[specs[pos].width * specs[pos].height * RGB];
+        for (int i = 0; i < specs[pos].width * specs[pos].height * RGB; i++) {
+            
+        }
+        images.push_back(pixels);
     } else {
-        pixels = new unsigned char[spec.width * spec.height * spec.nchannels];
+        pixels = new unsigned char[specs[pos].width * specs[pos].height * specs[pos].nchannels];
         input->read_image(TypeDesc::UINT8, pixels);
+        images.push_back(pixels);
     }
 
     std::string displayType;
-    for (auto &channelName : spec.channelnames) {
+    for (auto &channelName : specs[pos].channelnames) {
         displayType += channelName;
     }
 
     std::cout << "Image information: " << std::endl;
-    std::cout << "X RES   " << spec.width << std::endl;
-    std::cout << "Y RES   " << spec.height << std::endl;
-    std::cout << "CHANNEL " << spec.nchannels << std::endl;
+    std::cout << "X RES   " << specs[pos].width << std::endl;
+    std::cout << "Y RES   " << specs[pos].height << std::endl;
+    std::cout << "CHANNEL " << specs[pos].nchannels << std::endl;
     std::cout << "TYPE    " << displayType << std::endl;
     input->close();
 }
@@ -175,17 +224,17 @@ void loadImage(const std::string &inputFile) {
 /* Writes the image */
 void writeImage(const std::string &outputFile) {
     auto out = ImageOutput::create("Output.jpg");
-    ImageSpec specOut(spec.width, spec.height, spec.nchannels, TypeDesc::UINT8);
+    ImageSpec specOut(specs[state].width, specs[state].height, specs[state].nchannels, TypeDesc::UINT8);
     out->open(outputFile, specOut);
-    out->write_image(TypeDesc::UINT8, pixels);
+    out->write_image(TypeDesc::UINT8, images[state]);
     out->close();
 }
 
 /* Inverts the image */
 void invertImage() {
     unsigned char maxVal = 255;
-    for (int i = 0; i < (spec.width * spec.height * spec.nchannels); i++) {
-        pixels[i] = maxVal - pixels[i];
+    for (int i = 0; i < (specs[state].width * specs[state].height * specs[state].nchannels); i++) {
+        images[state][i] = maxVal - images[state][i];
     }
 }
 
@@ -193,7 +242,7 @@ void invertImage() {
  * that is used for displaying images */
 int getImagePixelTypeForGL() {
     std::string mainString;
-    for (auto &channelName : spec.channelnames) {
+    for (auto &channelName : specs[state].channelnames) {
         mainString += channelName;
     }
     if (mainString == "RGB") {
