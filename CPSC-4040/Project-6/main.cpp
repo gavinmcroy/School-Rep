3c332a526bf4d680382def4753e40a10b4b6e558
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2021-11-17 17:23:55 -0500

    mind numbing insanity

--- CPSC-4040/Project-6/main.cpp
+++ CPSC-4040/Project-6/main.cpp
@@ -4,6 +4,12 @@
 
 OIIO_NAMESPACE_USING
 
+void inv_map(float x, float y, float &u, float &v,
+             int inWidth, int inHeight, int outWidth, int outHeight);
+
+void inv_map2(float x, float y, float &u, float &v,
+              int inWidth, int inHeight, int outWidth, int outHeight);
+
 int main(int argc, char *argv[]) {
     const int EXPECTED_ARGS = 3;
     if (argc < EXPECTED_ARGS) {
@@ -24,24 +30,45 @@ int main(int argc, char *argv[]) {
     std::unique_ptr<unsigned char> image = std::unique_ptr<unsigned char>
             (new unsigned char[spec.height * spec.width * spec.nchannels]);
     image.get()[1] = 3;
-    inputFile->read_image(TypeDesc::UINT8,image.get());
+    inputFile->read_image(TypeDesc::UINT8, image.get());
 
     inputFile->close();
     return 0;
 }
 
+/*
+  Routine to inverse map (x, y) output image spatial coordinates
+  into (u, v) input image spatial coordinates
 
+  Call routine with (x, y) spatial coordinates in the output
+  image. Returns (u, v) spatial coordinates in the input image,
+  after applying the inverse map. Note: (u, v) and (x, y) are not
+  rounded to integers, since they are true spatial coordinates.
 
+  inwidth and inheight are the input image dimensions
+  outwidth and outheight are the output image dimensions
+*/
+void inv_map(float x, float y, float &u, float &v,
+             int inWidth, int inHeight, int outWidth, int outHeight) {
+    x /= outWidth;        // normalize (x, y) to (0...1, 0...1)
+    y /= outHeight;
 
+    u = x / 2;
+    v = y / 2;
 
+    u *= inWidth;            // scale normalized (u, v) to pixel coords
+    v *= inHeight;
+}
 
+void inv_map2(float x, float y, float &u, float &v,
+              int inWidth, int inHeight, int outWidth, int outHeight) {
 
+    x /= outWidth;        // normalize (x, y) to (0...1, 0...1)
+    y /= outHeight;
 
+    u = 0.5 * (x * x * x * x + sqrt(sqrt(y)));
+    v = 0.5 * (sqrt(sqrt(x)) + y * y * y * y);
 
-
-/* width = col. height = row */
-//    auto **pixmap = new RGB_PIXEL *[spec.height];
-//    pixmap[0] = new RGB_PIXEL[spec.width * spec.height];
-//    for(int i = 1; i < spec.height; i++){
-//        pixmap[i] = pixmap[i-1]+spec.width;
-//    }
+    u *= inWidth;            // scale normalized (u, v) to pixel coords
+    v *= inHeight;
+}
