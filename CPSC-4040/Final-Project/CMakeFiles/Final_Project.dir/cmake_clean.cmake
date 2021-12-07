f04f200a79494b1b806f6afdc54454b4e3ec413c
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2021-12-06 21:09:12 -0500

    anger

---
+++ CPSC-4040/Final-Project/CMakeFiles/Final_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_Project.dir/Cartoon.cpp.o"
+  "CMakeFiles/Final_Project.dir/Cartoon.cpp.o.d"
+  "CMakeFiles/Final_Project.dir/main.cpp.o"
+  "CMakeFiles/Final_Project.dir/main.cpp.o.d"
+  "Final_Project"
+  "Final_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Final_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
