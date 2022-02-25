d24e85eb5183b626129b703f18a3aaddc0ee010b
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2022-02-25 00:18:43 -0500

    cleaned up

--- Asg-1-3220/main.c
+++ Asg-1-3220/main.c
@@ -18,6 +18,7 @@ void grabCurrentPath();
 int main(int args, char *argv[]) {
     const int THREE_ARGS = 4;
     const char AREA_NAME[6] = "/area";
+    const char PERIMETER_NAME[12] = "/perimeter";
 
     Data data;
     if (args < THREE_ARGS) {
@@ -30,8 +31,8 @@ int main(int args, char *argv[]) {
     data.string = argv[3];
 
     /* Start up second process */
-    int error = fork();
-    if (error < 0) {
+    int firstChild = fork();
+    if (firstChild < 0) {
         fprintf(stderr, "Process failed to start\n");
         exit(1);
     }
@@ -39,15 +40,33 @@ int main(int args, char *argv[]) {
     /* execl area.c (print parent + child pid and result of calc then exit) */
     pid_t var1 = getpid();
     /* This is the child */
-    if (error == 0) {
-        char *temp = "./area";
-        printf("Child ID = %d \n", var1);
+    if (firstChild == 0) {
+        const int BUFFER = 256;
+        char *perimeterPath = (char *) malloc(sizeof(char) * BUFFER);
+        char *areaPath = (char *) malloc(sizeof(char) * BUFFER);
+
+        char *tempArea = "./area";
+        char *tempPerimeter = "./perimeter";
+        printf("Child ID1 = %d \n", var1);
         grabCurrentPath();
         /* Append file name on the end of directory */
-        strcat(cwd, AREA_NAME);
-        printf("%s", cwd);
-        int problemFound = execl(cwd, temp, argv[1], argv[2],(char *) (NULL));
-        printf("\n%d\n",problemFound);
+        strcpy(areaPath,cwd);
+        strcpy(perimeterPath,cwd);
+        strcat(areaPath, AREA_NAME);
+        strcat(perimeterPath,PERIMETER_NAME);
+
+        printf("%s", areaPath);
+        /* This fork is for the second child of parent */
+//        int secondChild = fork();
+//
+//        if (secondChild == 0) {
+//            pid_t var2 = getpid();
+//            printf("Child ID2 = %d \n", var2);
+//            // execl()
+//        }
+        /* This is the first child being ran */
+        execl(areaPath, tempArea, argv[1], argv[2], (char *) (NULL));
+
     } else {
         printf("Parent ID = %d \n", var1);
     }
@@ -55,7 +74,7 @@ int main(int args, char *argv[]) {
     wait(NULL);
 
     /* execl perimeter.c (print parent + child pid with x1,x2, print output, exit) */
-    
+
 
 
     /* Parent makes two threads */
