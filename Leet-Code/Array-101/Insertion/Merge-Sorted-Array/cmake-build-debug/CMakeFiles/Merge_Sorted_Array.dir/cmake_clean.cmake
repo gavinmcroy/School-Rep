90b97186a54bcc3d0701c284e489b55e51d0c4d6
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-06-05 23:07:30 -0400

    interesting problem

--- Leet-Code/Array-101/Insertion/Merge-Sorted-Array/cmake-build-debug/CMakeFiles/Merge_Sorted_Array.dir/cmake_clean.cmake
+++ Leet-Code/Array-101/Insertion/Merge-Sorted-Array/cmake-build-debug/CMakeFiles/Merge_Sorted_Array.dir/cmake_clean.cmake
@@ -1,7 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Merge_Sorted_Array.dir/main.cpp.o"
-  "Merge_Sorted_Array"
+  "CMakeFiles/Merge_Sorted_Array.dir/main.cpp.obj"
+  "CMakeFiles/Merge_Sorted_Array.dir/main.cpp.obj.d"
+  "Merge_Sorted_Array.exe"
+  "Merge_Sorted_Array.exe.manifest"
   "Merge_Sorted_Array.pdb"
+  "libMerge_Sorted_Array.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
